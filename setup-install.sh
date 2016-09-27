f11c7efd02cfdfe3e37f115e48fd51e80f97779f
jnaneshwar das <jnaneshwar das>
2016-09-26 17:40:48 -0700

    minor fix to setup-install script to run gitmodule update in the correct folder

--- setup-install.sh
+++ setup-install.sh
@@ -7,7 +7,7 @@ sudo usermod -a -G dialout $USER
 
 sudo add-apt-repository ppa:george-edison55/cmake-3.x -y
 sudo apt-get update
-sudo apt-get -q -y install cmake -y
+sudo apt-get -q -y install cmake vim -y
 sudo apt-get -q -y install ant protobuf-compiler libeigen3-dev libopencv-dev
 sudo apt-get -q -y install python-argparse git-core wget zip python-empy qtcreator cmake build-essential genromfs -y
 curl -ssL http://get.gazebosim.org | sh
@@ -20,8 +20,11 @@ sudo apt-get -q -y install python-serial openocd flex bison libncurses5-dev auto
     libftdi-dev libtool zlib1g-dev \
     python-empy gcc-arm-embedded -y
 
+cd ~
 mkdir src 
 cd src 
 git clone https://github.com/darknight-007/Firmware
+cd Firmware
 git submodule update --init --recursive
-cd ~
+echo "now type the following at the command prompt:"
+echo "make posix_sitl_default gazebo"
