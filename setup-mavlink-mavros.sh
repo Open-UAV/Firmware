6c4ea71f01c4d716b65795cc2433b078e698921e
Jnaneshwar Das <jnaneshwar.das@gmail.com>
2016-10-12 02:29:03 -0400

    Update setup-mavlink-mavros.sh

--- setup-mavlink-mavros.sh
+++ setup-mavlink-mavros.sh
@@ -9,6 +9,10 @@ rosinstall_generator --rosdistro kinetic mavlink | tee -a /tmp/mavros.rosinstall
 wstool merge -t src /tmp/mavros.rosinstall
 wstool update -t src
 rosdep install --from-paths src --ignore-src --rosdistro jade -y
-catkin build
+cd ~/catkin_ws/src/
+ln -s ~/src/Firmware/
+ln -s ~/src/Firmware/Tools/sitl_gazebo/
+catkin build 
 echo "source ~/catkin_ws/devel/setup.bash" >> ~/.bashrc
+
 
