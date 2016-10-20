f46fe3f593bcf1e93eb54d0efae8121d5fe17715
jnaneshwar das@ <jnaneshwar.das@gmail.com>
2016-10-20 02:19:38 -0400

    First draft of offboard position control code that attempts to change UAV dynamics in flight

--- testScripts/testOffboardPositionControlWithGainAndIntertialParamChange.py
+++ testScripts/testOffboardPositionControlWithGainAndIntertialParamChange.py
@@ -8,6 +8,11 @@ from geometry_msgs.msg import PoseStamped, Point, Quaternion
 import math
 import numpy
 from gazebo_msgs.srv import SetLinkProperties
+from gazebo_msgs.srv import SetLinkPropertiesRequest
+from gazebo_msgs.srv import GetLinkProperties
+from gazebo_msgs.srv import GetLinkPropertiesRequest
+from gazebo_msgs.srv import GetLinkPropertiesResponse
+
 
 
 
@@ -34,7 +39,9 @@ class OffboardPosCtlWithOnlineDynamicalUpdates:
         pose_pub = rospy.Publisher('/mavros/setpoint_position/local', PoseStamped, queue_size=10)
         mocap_sub = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, callback=self.mocap_cb)
         state_sub = rospy.Subscriber('/mavros/state', State, callback=self.state_cb)
-        gazebo_service = rospy.ServiceProxy('/gazebo/set_link_properties', SetLinkProperties)
+        gazebo_service_set_link_properties = rospy.ServiceProxy('/gazebo/set_link_properties', SetLinkProperties)
+        gazebo_service_get_link_properties = rospy.ServiceProxy('/gazebo/get_link_properties', GetLinkProperties)
+
         rate = rospy.Rate(10)  # Hz
         rate.sleep()
         self.des_pose = self.copy_pose(self.curr_pose)
@@ -42,7 +49,7 @@ class OffboardPosCtlWithOnlineDynamicalUpdates:
 
 
         while not rospy.is_shutdown():
-            print self.sim_ctr, shape[0], self.waypointIndex
+            #print self.sim_ctr, shape[0], self.waypointIndex
             if self.waypointIndex is shape[0]:
                 self.waypointIndex = 0
                 self.sim_ctr += 1
@@ -68,6 +75,23 @@ class OffboardPosCtlWithOnlineDynamicalUpdates:
                 dist = math.sqrt((curr_x - des_x)*(curr_x - des_x) + (curr_y - des_y)*(curr_y - des_y) + (curr_z - des_z)*(curr_z - des_z))
                 if dist < self.distThreshold:
                     self.waypointIndex += 1
+                    current_params = GetLinkPropertiesResponse()
+                    current_params = gazebo_service_get_link_properties.call(GetLinkPropertiesRequest('base_link'))
+                    des_params = current_params
+                    des_params = SetLinkPropertiesRequest()
+                    des_params.mass = current_params.mass+0.3
+                    des_params.gravity_mode = current_params.gravity_mode
+                    des_params.com = current_params.com
+                    des_params.ixx = current_params.ixx
+                    des_params.ixy = current_params.ixy
+                    des_params.ixz = current_params.ixz
+                    des_params.iyy = current_params.iyy
+                    des_params.iyz = current_params.ixz
+                    des_params.izz = current_params.izz
+                    des_params.link_name = 'base_link'
+                    print des_params
+
+                    gazebo_service_set_link_properties.call(des_params)
 
                 # print dist, curr_x, curr_y, curr_z, self.waypointIndex
             pose_pub.publish(self.des_pose)
