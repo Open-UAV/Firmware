c12c31e4f7a420e2c48a447b076fa03963cdd73b
jnaneshwar das <jnaneshwar.das@gmail.com>
2017-07-29 02:24:16 +0000

    can launch 4 uavs and set one set of position setpoints. still hacky offboard mode transition

--- testScripts/testMultiUAVCommanding.py
+++ testScripts/testMultiUAVCommanding.py
@@ -2,27 +2,41 @@ import rospy
 import roslaunch
 import subprocess
 import os
-
+import time
 from sensor_msgs.msg import LaserScan, NavSatFix
 from std_msgs.msg import Float64;
 from gazebo_msgs.msg import ModelStates
-
+from mavros_msgs.msg import State
+import sys 
 from mavros_msgs.srv import CommandBool, CommandTOL, SetMode
 from geometry_msgs.msg import PoseStamped,Pose,Vector3,Twist,TwistStamped
 from std_srvs.srv import Empty
-
+NUM_UAV=int(sys.argv[1])
 cur_pose = PoseStamped()
 def pos_cb(msg):
     global cur_pose
     cur_pose = msg
 
+def state_cb(msg):
+        for uavID in range(0, NUM_UAV):
 
-NUM_UAV=5
+                try:
+                        print mode_proxy[uavID](0,'OFFBOARD')
+                except rospy.ServiceException, e:
+                        print ("mavros/set_mode service call failed: %s"%e)
+       
+		try:
+			arm_proxy[uavID](True)
+    		except rospy.ServiceException, e:
+			print ("mavros1/set_mode service call failed: %s"%e)
+
+
 local_pos = [None for i in range(NUM_UAV)]
 mode_proxy = [None for i in range(NUM_UAV)]
 arm_proxy = [None for i in range(NUM_UAV)]
 pos_sub = [None for i in range(NUM_UAV)]
 start_pos = [None for i in range(NUM_UAV)]
+mavros_state = [None for i in range(NUM_UAV)]
 
 
 def mavrosTopicStringRoot(uavID=0):
@@ -32,7 +46,7 @@ startPosX = [5, 4, 4, 2, 3]
 startPosY = [0, 0, 0, 2, 3]
 startPosZ = [5, 5, 5, 5, 5]
 
-rospy.init_node('multi', anonymous=True)
+rospy.init_node('multi-', anonymous=True)
 
 
 for uavID in range(0,NUM_UAV):
@@ -40,6 +54,7 @@ for uavID in range(0,NUM_UAV):
     mode_proxy[uavID] = rospy.ServiceProxy(mavrosTopicStringRoot(uavID) + '/set_mode', SetMode)
     arm_proxy[uavID] = rospy.ServiceProxy(mavrosTopicStringRoot(uavID) + '/cmd/arming', CommandBool)
     pos_sub[uavID] = rospy.Subscriber(mavrosTopicStringRoot(uavID) + '/local_position/pose', PoseStamped, callback=pos_cb)
+    mavros_state[uavID] = rospy.Subscriber(mavrosTopicStringRoot(uavID) + '/state', State, callback=state_cb)
 
     start_pos[uavID] = PoseStamped()
     start_pos[uavID].pose.position.x = startPosX[uavID]
@@ -59,46 +74,18 @@ while None in data:
             pass
 
 for uavID in range(0, NUM_UAV):
-    print "wait for service"
-    rospy.wait_for_service(mavrosTopicStringRoot(uavID) + '/set_mode')
-    print "got service"
+    rospy.wait_for_service(mavrosTopicStringRoot(uavID) + '/cmd/arming')
 
-success = [None for i in range(NUM_UAV)]
 for uavID in range(0, NUM_UAV):
-    try:
-        print mode_proxy[uavID]
-        success[uavID] = mode_proxy[uavID](0,'OFFBOARD')
-        print 'mode '
-        print success[uavID]
-    except rospy.ServiceException, e:
-        print ("mavros/set_mode service call failed: %s"%e)
+    rospy.wait_for_service(mavrosTopicStringRoot(uavID) + '/set_mode')
 
-
+        
 
 #Main method
-rate = rospy.Rate(10)
+rate = rospy.Rate(200)
 print "Main Running"
 while not rospy.is_shutdown():
     for uavID in range(0, NUM_UAV):
-        local_pos[uavID].publish(start_pos[uavID])
-        print start_pos[uavID]
-    rate.sleep()
+    	local_pos[uavID].publish(start_pos[uavID])
+	rate.sleep()
 
-
-#Arm
-success = [None for i in range(NUM_UAV)]
-for uavID in range(0, NUM_UAV):
-    #Arm
-    print 'arming' + mavrosTopicStringRoot(uavID)
-    rospy.wait_for_service(mavrosTopicStringRoot(uavID) + '/cmd/arming')
-
-
-success = []
-for uavID in range(0, NUM_UAV):
-    try:
-       success[uavID] = arm_proxy[uavID](True)
-       print 'arming '
-       print success[uavID]
-    except rospy.ServiceException, e:
-       print ("mavros1/set_mode service call failed: %s"%e)
-       print "armed"
