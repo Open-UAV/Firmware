e5305611f9b02e7ce1a10806b26bd2e375758647
jnaneshwar das <jnaneshwar.das@gmail.com>
2017-07-30 16:15:32 +0000

    formation controller now reads formation setpoints directly. has no knowledge of state of mission yet

--- testScripts/testMultiUAVCommanding.py
+++ testScripts/testMultiUAVCommanding.py
@@ -12,13 +12,6 @@ from mavros_msgs.srv import CommandBool, CommandTOL, SetMode
 from geometry_msgs.msg import PoseStamped,Pose,PoseArray,Vector3,Twist,TwistStamped
 from std_srvs.srv import Empty
 NUM_UAV=int(sys.argv[1])
-cur_pose = PoseStamped()
-def pos_cb(msg):
-    global cur_pose
-    cur_pose = msg
-
-def formation_cb(msg):
-    print msg
 
 def state_cb(msg):
 	if(msg.armed == False):
@@ -43,35 +36,36 @@ local_pos = [None for i in range(NUM_UAV)]
 mode_proxy = [None for i in range(NUM_UAV)]
 arm_proxy = [None for i in range(NUM_UAV)]
 pos_sub = [None for i in range(NUM_UAV)]
+cur_pos = [None for i in range(NUM_UAV)]
 formation_setpoints = [None for i in range(NUM_UAV)]
 start_pos = [None for i in range(NUM_UAV)]
 mavros_state = [None for i in range(NUM_UAV)]
+
+def pos_cb(msg, args):
+    global cur_pos
+    cur_pos[args] = msg
+
 
 
 def mavrosTopicStringRoot(uavID=0):
     return ('mavros' + str(uavID+1))
 
-startPosX = [1, 0, 1, 0, 1]
-startPosY = [0, 1, 2, 3, 4]
-startPosZ = [3, 3, 3, 3, 3]
-
 rospy.init_node('multi-', anonymous=True)
-
+def formation_cb(msg,args):
+	global start_pos
+	global cur_pos
+	start_pos[args]= PoseStamped()
+	start_pos[args].header = cur_pos[uavID].header
+	start_pos[args].pose = msg.poses[6]
 
 for uavID in range(0,NUM_UAV):
     local_pos[uavID] = rospy.Publisher(mavrosTopicStringRoot(uavID) + '/setpoint_position/local', PoseStamped, queue_size=10)
     mode_proxy[uavID] = rospy.ServiceProxy(mavrosTopicStringRoot(uavID) + '/set_mode', SetMode)
     arm_proxy[uavID] = rospy.ServiceProxy(mavrosTopicStringRoot(uavID) + '/cmd/arming', CommandBool)
-    pos_sub[uavID] = rospy.Subscriber(mavrosTopicStringRoot(uavID) + '/local_position/pose', PoseStamped, callback=pos_cb)
-    formation_setpoints[uavID] = rospy.Subscriber(mavrosTopicStringRoot(uavID) + '/formation_setpoints', PoseArray, callback=formation_cb)
+    pos_sub[uavID] = rospy.Subscriber(mavrosTopicStringRoot(uavID) + '/local_position/pose', PoseStamped, pos_cb,(uavID))
+    formation_setpoints[uavID] = rospy.Subscriber(mavrosTopicStringRoot(uavID) + '/formation_setpoints', PoseArray, formation_cb,(uavID))
 
     mavros_state[uavID] = rospy.Subscriber(mavrosTopicStringRoot(uavID) + '/state', State, callback=state_cb)
-
-    start_pos[uavID] = PoseStamped()
-    start_pos[uavID].pose.position.x = startPosX[uavID]
-    start_pos[uavID].pose.position.y = startPosY[uavID]
-    start_pos[uavID].pose.position.z = startPosZ[uavID]
-    print uavID
 
 
 
