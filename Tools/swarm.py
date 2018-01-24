6690dd0a71bc8d716975bf838b56de4436469dad
Jnaneshwar Das <jnaneshwar.das@gmail.com>
2018-01-23 19:50:07 -0500

    Python script( in progress), to support creation of more than 9 UAVs

--- Tools/swarm.py
+++ Tools/swarm.py
@@ -3,32 +3,43 @@ import sys
 import os
 
 def replaceInFile(orig, repl, filename):
-    os.system('sed - i "s/' + orig + '/' +repl + '/g" ' + filename)
+    os.system('sed -i "s/' + orig + '/' +repl + '/g" ' + filename)
 
 NUM_UAVs = int(sys.argv[1])
 PX4_HOME = '/home/nsf/open-uav'
+print(NUM_UAVs)
 for NUM in range(1, NUM_UAVs):
-    fcu1 = 14140
+
+    # mavlink
+    # < mavlink_udp_port > 14660 < / mavlink_udp_port >
+    # simulator start -s -u 14660
+    # mavlink start -u 14656 -r 4000000
+    # mavlink start -u 14657 -r 4000000 -m onboard -o 14640
+    # <param name="fcu_url" value="udp://:mavlink_onboard_remote@localhost:mavlink_onboard_local"/>
+    # mavlink stream -r 50 -s POSITION_TARGET_LOCAL_NED -u 14656
+
+    mavlink_onboard_remote = 14140
     mavlink_start_port = 14156
-    fcu2 = 14157
-    mavlink_port = 14160
+    mavlink_onboard_local = 14157
+    simulator_udp_port = 14160
 
-uav_str = str(NUM)
-os.system(
-    "cp -r $PX4_HOME/Firmware/Tools/sitl_gazebo/models/f450-1 $PX4_HOME/Firmware/Tools/sitl_gazebo/models/f450-tmp-" + uav_str)
-os.system(
-    "mv $PX4_HOME/Firmware/Tools/sitl_gazebo/models/f450-tmp-$NUM/f450-1.sdf $PX4_HOME/Firmware/Tools/sitl_gazebo/models/f450-tmp-$NUM/f450-tmp-" + uav_str + ".sdf")
+    uav_str = str(NUM)
+    print(uav_str)
+    print(os.system(
+        "cp -r " + PX4_HOME + "/Firmware/Tools/sitl_gazebo/models/f450-1 " + PX4_HOME + "/Firmware/Tools/sitl_gazebo/models/f450-tmp-" + uav_str))
+    print(os.system(
+        "mv " + PX4_HOME + "/Firmware/Tools/sitl_gazebo/models/f450-tmp-" + uav_str +"/f450-1.sdf " + PX4_HOME + "/Firmware/Tools/sitl_gazebo/models/f450-tmp-" + uav_str +"/f450-tmp-" + uav_str + ".sdf"))
 
-replaceInFile(str(mavlink_port), str(mavlink_port+100+NUM), PX4_HOME + '/Firmware/Tools/sitl_gazebo/models/f450-tmp-$NUM/f450-tmp-" + NUM_UAVs + ".sdf')
-os.system(
-    'cp '+ PX4_HOME + '/Firmware/posix-configs/SITL/init/lpe/f450-1 $PX4_HOME/Firmware/posix-configs/SITL/init/lpe/f450-tmp-' +
-    uav_str)
+    replaceInFile(str(simulator_udp_port), str(simulator_udp_port+100*NUM), PX4_HOME + '/Firmware/Tools/sitl_gazebo/models/f450-tmp-' + uav_str +'/f450-tmp-' + uav_str + '.sdf')
+    os.system(
+        'cp '+ PX4_HOME + '/Firmware/posix-configs/SITL/init/lpe/f450-1 ' + PX4_HOME + '/Firmware/posix-configs/SITL/init/lpe/f450-tmp-' +
+        uav_str)
 
-replaceInFile(str(mavlink_port), str(mavlink_port+100+NUM), PX4_HOME + '/Firmware/posix-configs/SITL/init/lpe/f450-tmp-' + uav_str)
-replaceInFile('f450-1', 'f450-tmp-' + uav_str, PX4_HOME + '/Firmware/Tools/sitl_gazebo/models/f450-tmp-$NUM/f450-tmp-' + uav_str)
-replaceInFile('uav_camera',
-              'uav_' + uav_str + '_camera',
-              PX4_HOME + '/Firmware/Tools/sitl_gazebo/models/f450-tmp-' + uav_str + '/f450-tmp-' + uav_str + '.sdf')
+    replaceInFile(str(simulator_udp_port), str(simulator_udp_port+100*NUM), PX4_HOME + '/Firmware/posix-configs/SITL/init/lpe/f450-tmp-' + uav_str)
+    replaceInFile('f450-1', 'f450-tmp-' + uav_str, PX4_HOME + '/Firmware/Tools/sitl_gazebo/models/f450-tmp-' + uav_str +'/f450-tmp-' + uav_str + '.sdf')
+    replaceInFile('uav_camera',
+                  'uav_' + uav_str + '_camera',
+                  PX4_HOME + '/Firmware/Tools/sitl_gazebo/models/f450-tmp-' + uav_str + '/f450-tmp-' + uav_str + '.sdf')
 
 launch_file = '$PX4_HOME/Firmware/launch/posix_sitl_multi_tmp.launch'
 
@@ -36,32 +47,31 @@ SOURCE = PX4_HOME + '/Firmware/launch/posix_sitl_multibase.launch'
 DEST = PX4_HOME + '/Firmware/launch/posix_sitl_multi_tmp.launch'
 
 file_block = ''
-for ind in range(1, NUM_UAVs):
+for NUM in range(1, NUM_UAVs):
     uav_block = '<arg name="x1" default="0"/>' + \
-                '<arg name="y' + str(ind) + '" default="1"/>' + \
-                '<arg name="vehicle' + str(ind) + '" default="f450-tmp-1"/>' + \
+                '<arg name="y' + str(NUM) + '" default="1"/>' + \
+                '<arg name="vehicle' + str(NUM) + '" default="f450-tmp-1"/>' + \
                 '<arg name="sdf' + str(
-        ind) + '" default="$(find mavlink_sitl_gazebo)/models/$(arg vehicle1)/$(arg vehicle1).sdf"/>' + \
+        NUM) + '" default="$(find mavlink_sitl_gazebo)/models/$(arg vehicle1)/$(arg vehicle1).sdf"/>' + \
                 '<arg name="rcS' + str(
-        ind) + '" default="$(find px4)/posix-configs/SITL/init/$(arg est)/$(arg vehicle1)"/>' + \
+        NUM) + '" default="$(find px4)/posix-configs/SITL/init/$(arg est)/$(arg vehicle1)"/>' + \
                 '<node name="sitl' + str(
-        ind) + '" pkg="px4" type="px4" output="screen" args="$(find px4) $(arg rcS1)"></node>' + \
+        NUM) + '" pkg="px4" type="px4" output="screen" args="$(find px4) $(arg rcS1)"></node>' + \
                 '<node name="$(anon vehicle_spawn_' + str(
-        ind) + ')" output="screen" pkg="gazebo_ros" type="spawn_model"' + \
-                'args="-sdf -file $(arg sdf' + str(ind) + ') -model $(arg vehicle1) -x $(arg x' + str(
-        ind) + '=) -y $(arg y' + str(
-        ind) + ') -z $(arg z) -R $(arg R) -P $(arg P) -Y $(arg Y)"/>-->'
+        NUM) + ')" output="screen" pkg="gazebo_ros" type="spawn_model"' + \
+                'args="-sdf -file $(arg sdf' + str(NUM) + ') -model $(arg vehicle1) -x $(arg x' + str(
+        NUM) + '=) -y $(arg y' + str(
+        NUM) + ') -z $(arg z) -R $(arg R) -P $(arg P) -Y $(arg Y)"/>-->'
     file_block = file_block + '\n' + uav_block
 
-for ind in range(1, NUM_UAVs):
-    port1 = '14014'
-    port2 = '14050'
-    mavros_block = '<node pkg="mavros" type="mavros_node" name="mavros'+ str(ind) +'" required="true" clear_params="true" output="$(arg log_output)"> \
-		<param name="fcu_url" value="udp://:'+ port1+'@localhost:'+port2+'"/> \
+for NUM in range(1, NUM_UAVs):
+
+    mavros_block = '<node pkg="mavros" type="mavros_node" name="mavros'+ str(NUM) +'" required="true" clear_params="true" output="$(arg log_output)"> \
+		<param name="fcu_url" value="udp://:' + str(mavlink_onboard_remote+100*NUM) + '@localhost:' + str(mavlink_onboard_local+100*NUM) + '"/> \
 		<param name="gcs_url" value="" /> \
-		<param name="target_system_id" value="'+str(ind)+'" /> \
-		<param name="target_component_id" value="'+str(ind)+'" /> \
-		<param name="system_id" value="'+str(ind+30)+'" /> \
+		<param name="target_system_id" value="'+str(NUM)+'" /> \
+		<param name="target_component_id" value="'+str(NUM)+'" /> \
+		<param name="system_id" value="'+str(NUM+30)+'" /> \
 		<rosparam command="load" file="$(arg pluginlists_yaml)" /> \
 		<rosparam command="load" file="$(arg config_yaml)" /> \
     </node>'
