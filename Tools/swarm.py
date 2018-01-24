ce1bcbc119d45bd539b547f9669988e947bcfa73
Jnaneshwar Das <jnaneshwar.das@gmail.com>
2018-01-23 20:20:39 -0500

    Python script( in progress), to support creation of more than 9 UAVs

--- Tools/swarm.py
+++ Tools/swarm.py
@@ -5,18 +5,18 @@ import os
 def replaceInFile(orig, repl, filename):
     os.system('sed -i "s/' + orig + '/' +repl + '/g" ' + filename)
 
-NUM_UAVs = int(sys.argv[1])
+NUM_UAVs = int(sys.argv[1]) + 1
 PX4_HOME = '/home/nsf/open-uav'
 print(NUM_UAVs)
 for NUM in range(1, NUM_UAVs):
 
     # mavlink
-    # < mavlink_udp_port > 14660 < / mavlink_udp_port >
-    # simulator start -s -u 14660
-    # mavlink start -u 14656 -r 4000000
-    # mavlink start -u 14657 -r 4000000 -m onboard -o 14640
+    # < mavlink_udp_port > simulator_udp_port < / mavlink_udp_port >
+    # simulator start -s -u simulator_udp_port
+    # mavlink start -u mavlink_start_port -r 4000000
+    # mavlink start -u mavlink_onboard_local -r 4000000 -m onboard -o mavlink_onboard_remote
     # <param name="fcu_url" value="udp://:mavlink_onboard_remote@localhost:mavlink_onboard_local"/>
-    # mavlink stream -r 50 -s POSITION_TARGET_LOCAL_NED -u 14656
+    # mavlink stream -r 50 -s POSITION_TARGET_LOCAL_NED -u mavlink_start_port
 
     mavlink_onboard_remote = 14140
     mavlink_start_port = 14156
@@ -52,15 +52,15 @@ for NUM in range(1, NUM_UAVs):
                 '<arg name="y' + str(NUM) + '" default="' + str(NUM) +'"/>\n' + \
                 '<arg name="vehicle' + str(NUM) + '" default="f450-tmp-' + str(NUM) + '"/>\n' + \
                 '<arg name="sdf' + str(
-        NUM) + '" default="$(find mavlink_sitl_gazebo)/models/$(arg vehicle' + str(NUM) + ')/$(arg vehicle1).sdf"/>\n' + \
+        NUM) + '" default="$(find mavlink_sitl_gazebo)/models/$(arg vehicle' + str(NUM) + ')/$(arg vehicle' + str(NUM) + ').sdf"/>\n' + \
                 '<arg name="rcS' + str(
         NUM) + '" default="$(find px4)/posix-configs/SITL/init/$(arg est)/$(arg vehicle' + str(NUM) + ')"/>\n' + \
                 '<node name="sitl' + str(
         NUM) + '" pkg="px4" type="px4" output="screen" args="$(find px4) $(arg rcS' + str(NUM) + ')"></node>\n' + \
                 '<node name="$(anon vehicle_spawn_' + str(
         NUM) + ')" output="screen" pkg="gazebo_ros" type="spawn_model"\n' + \
-                'args="-sdf -file $(arg sdf' + str(NUM) + ') -model $(arg vehicle' + str(NUM) + ') -x $(arg x\n' + str(
-        NUM) + '=) -y $(arg y' + str(
+                'args="-sdf -file $(arg sdf' + str(NUM) + ') -model $(arg vehicle' + str(NUM) + ') -x $(arg x' + str(
+        NUM) + ') -y $(arg y' + str(
         NUM) + ') -z $(arg z) -R $(arg R) -P $(arg P) -Y $(arg Y)"/>\n'
     file_block = file_block + '\n' + uav_block
 
@@ -77,5 +77,9 @@ for NUM in range(1, NUM_UAVs):
     </node>'
     file_block = file_block + '\n' + mavros_block
 print(file_block)
+print(os.system("cp " + SOURCE + " " + DEST))
+f=open(DEST,"a")
+f.write(file_block + '\n </launch>')
+f.close()
 
 print('DRONES CREATED')
